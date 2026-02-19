6d859d9057931bfb9a68d215f49ff53a27af6bbd
Roger Leigh <roger.leigh@oregonstate.edu>
2026-02-19 12:38:16 -0800

    fix sites to include agsci.dev.o.e

--- docroot/sites/sites.php
+++ docroot/sites/sites.php
@@ -1,7 +1,11 @@
 <?php
+$sites['agsci.oregonstate.edu'] = 'agsci.oregonstate.edu';
+$sites['agsci.dev.oregonstate.edu'] = 'agsci.oregonstate.edu';
+$sites['dev10.agsci.oregonstate.edu'] = 'agsci.oregonstate.edu';
+
 $sites['anrs.oregonstate.edu'] = 'agsci.oregonstate.edu';
 $sites['dev.anrs.oregonstate.edu'] = 'agsci.oregonstate.edu';
-$sites['dev10.agsci.oregonstate.edu'] = 'agsci.oregonstate.edu';
 $sites['dev10.anrs.oregonstate.edu'] = 'agsci.oregonstate.edu';
+
 $sites['landscapeplants.dev.oregonstate.edu'] = 'landscapeplants.oregonstate.edu';
 $sites['landscapeplants.stage.oregonstate.edu'] = 'landscapeplants.oregonstate.edu';
