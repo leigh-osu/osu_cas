1fc99146ec7755059c67857aac2a54f15a981345
Roger Leigh <roger.leigh@oregonstate.edu>
2026-03-04 20:55:00 -0800

    manzanita theme, config sync dir added

--- docroot/sites/agsci.oregonstate.edu/settings.php
+++ docroot/sites/agsci.oregonstate.edu/settings.php
@@ -889,6 +889,8 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
  * Keep this code block at the end of this file to take full effect.
  */
 
+$settings['config_sync_directory'] = '../config/agsci-oregonstate-edu';
+
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
